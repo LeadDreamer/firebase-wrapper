2e809d1b0449c2fdd332a365d5707754e96e9109
Tracy Hall <LeadDreamer@users.noreply.github.com>
2021-05-24 18:40:05 -0700

    dependencies version 1.0.33

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/storage";
 import "firebase/auth";
+import "firebaseui";
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 import "firebase/functions";
 
